ef61673e1fe81abf19a859400c28124fd175b590
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-17 12:04:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function Scooters(year,color,model){
   this.color=color;
   this.model=model;
 }
+function Scooters(year,color,model){
+  
+  this.year=year;
+  this.color=color;
+  this.model=model;
+}
