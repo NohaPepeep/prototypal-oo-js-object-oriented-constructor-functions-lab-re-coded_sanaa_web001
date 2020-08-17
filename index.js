00288857b638b860bd56b2532a8b8a926282e3b6
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-17 12:01:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function Scooters(year,color,model){
-  return{
-  year,
-  color,
-  model,}
+  
+  this.year=year;
+  this.color=color;
+  this.model=model;
 }
 let Scuber =new Scooters(1999,"pink","toyota")
